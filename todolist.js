431c8338e69a0f8b4079fbac4a79d49aafb1f1e2
zhangyi <3510907376@qq.com>
2019-06-06 20:03:26 +0800

    修改

--- todolist.js
+++ todolist.js
@@ -1,7 +1,7 @@
 window.addEventListener('load',function () {
     let tab=document.querySelectorAll('.tab>li');
     let prev=0;
-    let content=document.querySelector('.content');
+    let list=document.querySelector('.content');
     let type='all';
     let flag={all:'all',done:true,doing:false}
     let todolist=[
@@ -22,95 +22,101 @@ window.addEventListener('load',function () {
     localStorage.getItem('todolist')? JSON.parse(localStorage.getItem('todolist')):todolist;
 
 
-
     tab.forEach(function (ele,index) {
-        ele.onclick=function () {
-            tab[prev].classList.remove('hot');
-            this.classList.add('hot');
-            prev=index;
-            type=this.getAttribute('type');
-
+        ele.onclick = function () {
+            tab[prev].classList.remove("hot");
+            this.classList.add("hot");
+            prev = index;
+            type = this.getAttribute("type");
             render(filterData(type));
+
         }
     })
+    render(todolist);
     tab[0].onclick();
+    //修改
+    list.onclick = function (e) {
 
-    content.onclick=function (e) {
-        let arr=[];
-        let target=e.target;
-        let id=target.parentNode.id;
-        if (target.nodeName === 'INPUT') {
+        let target = e.target;
+        let id = target.parentNode.id;
+        if (target.nodeName ==="INPUT"){
             let ele = todolist.filter(ele=>ele.id == id)[0];
             console.log(ele);
             ele.status =target.checked;
-        } else if (target.nodeName==='DEL'){
-            let index=todolist.findIndex(ele=>ele.id===id);
-
+        } else if (target.nodeName ==="DEL"){
+            let index = todolist.findIndex(ele=>ele.id == id);
             todolist.splice(index,1);
-
-            render(filterData(type));
         }
-    }
-//    增加列表
-    let forms=document.forms[0];
-    let textBtn=forms.elements['content'];
-    let submitBtn=forms.elements[1];
-
-    submitBtn.onclick=function (e) {
-        e.preventDefault();
-        let obj=createObj();
-        todolist.push(obj);
-        forms.reset();
-        render(filterData(type));
         saveData();
-    };
-//    本地存储
-    function saveData() {
-        localStorage.setItem("todolist",JSON.stringify(todolist))
+        render(filterData(type));
     }
 
-    function createObj() {
-        let id=todolist[todolist.length-1].id+1;
-        let content=textBtn.value;
-        let ctime=new Date().toLocaleDateString();
-        let status=false;
-        return{id,content,ctime,status}
-    }
-    function filterData(type) {
-        let arr=[];
+    function  filterData(type) {
+        let arr =[];
         switch (type) {
-            case 'all':
-                arr=todolist;
+            case "all":
+                arr = todolist;
                 break;
-            case 'done':
-                arr=todolist.filter(ele=>ele.status)
+            case "done":
+                arr = todolist.filter(function (ele) {
+                    return ele.status;
+                });
                 break;
-            case 'doing':
-                arr=todolist.filter(ele=>!ele.status)
+            case "doing":
+                arr = todolist.filter(function (ele) {
+                    return !ele.status;
+                });
                 break;
-
         }
         return arr;
     }
-//    渲染列表
-    function render(arr) {
-        let html='';
-        arr.forEach(ele=>{
-            if (ele.status) {
-                html+=`
-                 <li id="${ele.id}">
-                   <input type="checkbox" checked> <p>${ele.content}</p> <del>X</del><time>${ele.ctime}</time>
-                 </li>
-                `;
-            }else {
-                html+=`
-                <li id="${ele.id}">
-                  <input type="checkbox" checked> <p>${ele.content}</p> <del>X</del><time>${ele.ctime}</time>
-                </li>
-                `;
-            }
 
+    //    增加
+    let form = document.forms[0];
+    let contentBtn = form.elements[0];
+    let submitBtn = form.elements[1];
+    submitBtn .onclick = function (e) {
+        e.preventDefault();
+        let obj = createobj();
+        todolist.push(obj);
+        form.reset();
+        render(filterData(type))
+        saveData();
+    };
+    function createobj() {
+        let id = todolist[todolist.length-1].id+1;
+        let content = contentBtn.value;
+        let ctime = new Date().toLocaleDateString();
+        let status = false;
+        return{id,content,ctime,status};
+    }
+    //    本地存储
+    function saveData(){
+        localStorage.setItem("todolist",JSON.stringify(todolist))
+    }
+    //    渲染
+    function render(arr) {
+        let html =``;
+        arr.forEach(function (ele,index) {
+            if (ele.status) {
+                html +=`
+                   <li id ='${ele.id}'>
+                       <input type="checkbox" checked>
+                       <p>${ele.content}</p>
+                       <del>✘</del>
+                       <time>${ele.ctime}</time>
+                   </li>`;
+            }else {
+                html +=`
+                   <li id ='${ele.id}'>
+                       <input type="checkbox">
+                       <p>${ele.content}</p>
+                       <del>✘</del>
+                       <time>${ele.ctime}</time>
+                       
+                   </li>`;
+            }
         })
-        content.innerHTML=html;
+        list.innerHTML = html;
     }
 })
